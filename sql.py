f9b2d68eda1ec73c77110ddd889bad9ec7970603
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2025-06-23 17:14:56 +0530

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,33 +1,24 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
 class BankAccount:
-    def __init__(self):
-        self.account_number = None
-        self.account_type = None
-        self.account_owner_name = None
-        self.account_owner_ssn = None
-        self.balance = 0.0
-
-    def set_account_number(self, number):
+    def __init__(self, number, acc_type, name, ssn, balance):
         self.account_number = number
-
-    def set_account_type(self, acc_type):
         self.account_type = acc_type
-
-    def set_account_owner_name(self, name):
         self.account_owner_name = name
-
-    def set_account_owner_ssn(self, ssn):
         self.account_owner_ssn = ssn
+        self.balance = balance
 
-    def set_balance(self, amount):
-        self.balance = amount
-
-
-def create_bank_account(account_number, account_type, account_name, account_ssn, balance):
-    account = BankAccount()
-    account.set_account_number(account_number)
-    account.set_account_type(account_type)
-    account.set_account_owner_name(account_name)
-    account.set_account_owner_ssn(account_ssn)
-    account.set_balance(balance)
-
-    return account
+@app.route("/create-account", methods=["POST"])
+def create_account():
+    # ❌ No authentication check here
+    data = request.json
+    account = BankAccount(
+        number=data["accountNumber"],
+        acc_type=data["accountType"],
+        name=data["accountName"],
+        ssn=data["accountSSN"],
+        balance=data["balance"]
+    )
+    return jsonify({"message": "Account created", "account": account.__dict__})
